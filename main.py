38935eeee2ec9ed535ef784df35a4cb5a1d7776a
Asude112 <146875596+Asude112@users.noreply.github.com>
2024-04-06 09:21:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from bot_token import *
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def detect(ctx):
+    await ctx.send("detect komutu çalıştı")
+
+bot.run("token")
